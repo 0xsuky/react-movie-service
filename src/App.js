9779a206a133d7c25ffd84b0790dadad524205a7
suky <0xsuky@gmail.com>
2021-10-09 23:49:54 +0900

    Feat: #2.3 구현
    map에 대하여 따로 renderFood 함수를 추출한 뒤 구현해봤음
    React에서는 key를 통해 유일함을 증명함 (key는 리액트 내부에서 사용)
    React에서는 img 태그 내 alt를 통해 접근성을 높이도록 권장

--- src/App.js
+++ src/App.js
@@ -4,32 +4,37 @@ function Food({ name, picture }) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: 'Kimchi',
     image: 'http://www.bibigo.com/img/en/img_food_kimchi1_1.jpg',
   },
   {
+    id: 2,
     name: 'Samgyeopsal',
     image:
       'https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg?type=w1200',
   },
   {
+    id: 3,
     name: 'Bibimbap',
     image:
       'https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjRfMjky/MDAxNDkzMDIzMjY5MjY5.u5q3w1Cl1wjxJoRzSpczd_VT2rScn_zaDV5LfKVMa6Mg.Tz5B-6i_SYH4yydceeZT6bNwDs-P5srXOE2uCDQ_Disg.JPEG.estelle926/151435979-56a57a083df78cf772888a61.jpg?type=w800',
   },
   {
+    id: 4,
     name: 'Doncasu',
     image:
       'http://th2.tmon.kr/thumbs/image/423/336/e17/1b1492318_700x700_95_FIT.jpg',
   },
   {
+    id: 5,
     name: 'Kimbab',
     image:
       'https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg',
@@ -39,9 +44,8 @@ const foodILike = [
 function App() {
   return (
     <div>
-      <h1>Hello!!!!!</h1>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
